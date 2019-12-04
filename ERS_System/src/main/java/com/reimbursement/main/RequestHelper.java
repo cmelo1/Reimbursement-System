478d963e3d99a60df8cd7c5d39162873ec2821ab
Caio Melo <demelocaio@gmail.com>
2019-12-04 11:03:35 -0500

    Fixed pathing.

--- ERS_System/src/main/java/com/reimbursement/main/RequestHelper.java
+++ ERS_System/src/main/java/com/reimbursement/main/RequestHelper.java
@@ -4,25 +4,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class RequestHelper {
-	
-	//The idea behind this class is to route requests based on the URI that comes along with the request.
-	
+
+	// The idea behind this class is to route requests based on the URI that comes
+	// along with the request.
+
 	public static String process(HttpServletRequest request, HttpServletResponse response) {
-		
-		switch(request.getRequestURI()) {
-		case "/ERS_System/HTML/index2.do": //this is the login file, the path might be fucked up.
-		return LoginController.login(request);
-		
-		//case "/Reimbursement_System_ERS_System/Register.do":
-		//return RegisterController.Register(request);
-		
-		case "/ERS_System/employee.do":
-		return EmployeeController.Home(request, response); //Home is where they should go if the login works.
+
+		switch (request.getRequestURI()) {
+		case "/ERS_System/HTML/index2.do": // this is the login file, the path might be fucked up.
+			return LoginController.login(request);
+
+		case "/ERS_System/HTML/employee.do"://recently changed
+			return EmployeeController.Home(request, response); // Home is where they should go if the login works.
 		
 		default:
 			return "/HTML/index2.html";
 		}
-		
+
 	}
 
 }
