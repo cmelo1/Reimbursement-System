2e75f13468f9984bc549a75a22429336bd69aa3c
Caio Melo <demelocaio@gmail.com>
2019-12-05 09:44:47 -0500

    Add files via upload

--- ERS_System/src/main/webapp/JavaScript/employee.js
+++ ERS_System/src/main/webapp/JavaScript/employee.js
@@ -1,5 +1,5 @@
 /**
- * 
+ * implement a button.onclick with the submission of ticket, 
  */
 
 window.onload = function(){
@@ -13,16 +13,33 @@ function getUserInfo(){
 		console.log(xhttp.readyState);
 		console.log(xhttp.status);
 		if(xhttp.readyState == 4 && xhttp.status == 200){
-			let user = JSON.parse(xhttp.responseText);
+			let user = JSON.parse(xhttp.responseText); //The response gets turned into this.
 			setValues(user); //Call set values function defined below
 		}
 	}
 
+	//this goes straight to the request helper.
 xhttp.open("GET",'http://localhost:8080/ERS_System/HTML/employee.do',true); //Sends the set values 'request' to this.
 xhttp.send();
+}
+
+//How to parse an array?
+function getTicketInfo(){ //not in use lol
+	let xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		console.log(xhttp.readyState);
+		console.log(xhttp.status);
+		if(xhttp.readyState == 4 && xhttp.status == 200){
+			let user = JSON.parse(xhttp.responseText); //The response gets turned into this.
+			setValues(user); //Call set values function defined below
+		}
+	}
 }
 
 function setValues(user){
 	document.getElementById("username").innerHTML = "User's username is" + user.username; 
 	document.getElementById("password").innerHTML = "User's password is" + user.password;
+	
+	
+	
 } //this will manipulate the elements on the next page.
