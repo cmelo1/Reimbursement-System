3d95e16894f0f87402e23686fe0d86b349d317a2
Caio Melo <demelocaio@gmail.com>
2019-12-07 15:22:05 -0500

    Implement Approve/Deny into ticket controllers,DAO

--- ERS_System/src/main/java/com/reimbursement/main/TicketController.java
+++ ERS_System/src/main/java/com/reimbursement/main/TicketController.java
@@ -1,6 +1,8 @@
 package com.reimbursement.main;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Blob;
 import java.sql.Timestamp;
 import java.util.Date;
@@ -85,5 +87,62 @@ public class TicketController { //NEEDS TO BE WORKED ON LOL
 		}
 		return null;
 	}
+	
+public static String displayAllTickets(HttpServletRequest request,HttpServletResponse response) {
+		//Sessions - Marshalling Tool
+		
+	ERS_User EmployeeUser = (ERS_User)request.getSession().getAttribute("CurrentUser");
+		TicketDAO tick = new TicketDAO();
+		ERS_Ticket[] ticketList = tick.selectAllTickets();
+		
+		try {
+			response.getWriter().write(new ObjectMapper().writeValueAsString(ticketList)); //returning an array 
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
+
+public static String approveTicket(HttpServletRequest request) {
+	TicketDAO tick = new TicketDAO();
+	BufferedReader br;
+	try {
+		br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+		String data = br.readLine();
+		System.out.println("DATA IN APPROVE TICKETS:" + data);
+		tick.approveTicket(data);
+	
+	} catch (IOException e) {
+		System.out.println("Something went wrong in approveTicket controller");
+		e.printStackTrace();
+	}
+	//request.getParameter(name)
+	System.out.println("Successfully approved ticket!");
+	return "/HTML/Manager.html";
+		
 }
+public static String denyTicket(HttpServletRequest request) {
+	TicketDAO tick = new TicketDAO();
+	BufferedReader br;
+	try {
+		br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+		String data = br.readLine();
+		System.out.println("DATA IN DENY TICKETS:" + data);
+		tick.denyTicket(data);
+	
+	} catch (IOException e) {
+		System.out.println("Something went wrong in denyTicket controller");
+		e.printStackTrace();
+	}
+	System.out.println("Successfully denied ticket!");
+	return "/HTML/Manager.html";
+	
+	
+}
+	
+}
+
+
